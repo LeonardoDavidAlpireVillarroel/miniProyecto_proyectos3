e176651b68f3f31f73288c934ac1100e61b5736e
Leonardo David Alpire Villarroel <leonardodalpirev@gmail.com>
2025-03-16 02:44:14 -0400

    Creacion script disparo al apuntar

--- MiniProyecto_ProyectosIII/Assets/Scripts/PlayerController.cs
+++ MiniProyecto_ProyectosIII/Assets/Scripts/PlayerController.cs
@@ -1,156 +1,11 @@
 using UnityEngine;
 using Unity.Cinemachine; // Asegúrate de incluir el espacio de nombres de Cinemachine
 using UnityEngine.InputSystem;
+using System;
 
 [RequireComponent(typeof(CharacterController), typeof(PlayerInput))]
 public class PlayerController : MonoBehaviour
 {
-    //public float speed = 5f;
-    //public float gravity = -9.8f;
-    //public float jumpSpeed = 15f;
-    //public float rotationSpeed = 100f; // Velocidad de rotación vertical
-    //public float verticalRotationLimit = 80f; // Límite de rotación vertical en grados
-
-    //private Vector3 velocity;
-    //private float hInput, vInput;
-    //private float verticalRotation = 0f;
-
-    //[SerializeField] private LayerMask groundMask;
-    //private Vector3 spherePos;
-
-    //private CharacterController characterController;
-    //private Vector3 moveDirection;
-    //[SerializeField] private float groundOffset;
-
-    //public CinemachineCamera virtualCamera; // Variable pública para la cámara virtual
-    //private Animator characterAnimator;
-
-    //void Awake()
-    //{
-    //    characterController = GetComponent<CharacterController>();
-    //    characterAnimator = GetComponent<Animator>();
-    //    Cursor.lockState = CursorLockMode.Locked; // Bloquea el cursor al centro de la pantalla
-    //}
-
-    //void Update()
-    //{
-    //    GetDirectionAndMove();
-    //    ApplyGravity();
-    //}
-
-    //void GetDirectionAndMove()
-    //{
-    //    hInput = Input.GetAxis("Horizontal");
-    //    vInput = Input.GetAxis("Vertical");
-
-
-    //    float currentSpeed = Input.GetKey(KeyCode.LeftShift) ? speed * 1.5f : speed; // Aumenta la velocidad al presionar Shift
-
-    //    if (IsGrounded())
-    //    {
-    //        velocity.y = -2f;
-
-    //        // Obtener la rotación de la cámara virtual
-    //        if (virtualCamera != null)
-    //        {
-    //            Transform cam = virtualCamera.transform;
-
-    //            // Rotación horizontal del jugador
-    //            transform.rotation = Quaternion.Euler(0, cam.eulerAngles.y, 0);
-
-    //            // Rotación vertical del jugador (limitada)
-    //            verticalRotation -= Input.GetAxis("Mouse Y") * rotationSpeed;
-    //            verticalRotation = Mathf.Clamp(verticalRotation, -verticalRotationLimit, verticalRotationLimit);
-
-    //            // Aplicar la rotación vertical a la cámara virtual
-    //            cam.localEulerAngles = new Vector3(verticalRotation, cam.localEulerAngles.y, cam.localEulerAngles.z);
-
-    //            // Movimiento en relación con la dirección de la cámara virtual
-    //            Vector3 forward = new Vector3(cam.forward.x, 0, cam.forward.z).normalized;
-    //            Vector3 right = new Vector3(cam.right.x, 0, cam.right.z).normalized;
-
-    //            moveDirection = (forward * vInput + right * hInput).normalized * currentSpeed;
-    //            CharacterAnimations();
-
-    //            if (Input.GetButtonDown("Jump"))
-    //            {
-    //                velocity.y = jumpSpeed;
-    //            }
-    //        }
-
-    //        characterController.Move((moveDirection + velocity) * Time.deltaTime);
-
-    //        // Verificar si el jugador se está moviendo y actualizar la animación
-
-    //    }
-
-    //}
-    //void CharacterAnimations()
-    //{
-    //    if (moveDirection == Vector3.zero)
-    //    {
-
-    //        characterAnimator.SetFloat("speed", 0f);
-
-    //        if (Input.GetKey(KeyCode.Mouse1)) // Click derecho del mouse
-    //        {
-    //            characterAnimator.SetBool("isAiming", true);
-    //        }
-    //        else
-    //        {
-    //            characterAnimator.SetBool("isAiming", false);
-    //        }
-    //    }
-    //    else if (Input.GetKey(KeyCode.LeftShift))
-    //    {
-    //        characterAnimator.SetFloat("speed", 2f);
-    //        if (Input.GetKey(KeyCode.Mouse1)) // Click derecho del mouse
-    //        {
-    //            characterAnimator.SetBool("isAiming", true);
-    //        }
-    //        else
-    //        {
-    //            characterAnimator.SetBool("isAiming", false);
-    //        }
-    //    }
-    //    else
-    //    {
-    //        if (Input.GetKey(KeyCode.Mouse1)) // Click derecho del mouse
-    //        {
-    //            characterAnimator.SetBool("isAiming", true);
-    //        }
-    //        else
-    //        {
-    //            characterAnimator.SetBool("isAiming", false);
-    //        }
-    //        characterAnimator.SetFloat("speed", 1f);
-    //    }
-    //}
-
-    //void ApplyGravity()
-    //{
-    //    if (!IsGrounded())
-    //    {
-    //        velocity.y += gravity * Time.deltaTime;
-    //    }
-    //}
-
-    //private void OnDrawGizmos()
-    //{
-    //    if (characterController != null)
-    //    {
-    //        Gizmos.color = Color.red;
-    //        spherePos = new Vector3(transform.position.x, transform.position.y - characterController.height / 2 + groundOffset, transform.position.z);
-    //        Gizmos.DrawWireSphere(spherePos, characterController.radius - 0.05f);
-    //    }
-    //}
-
-    //bool IsGrounded()
-    //{
-    //    spherePos = new Vector3(transform.position.x, transform.position.y - characterController.height / 2 + groundOffset, transform.position.z);
-    //    return Physics.CheckSphere(spherePos, characterController.radius - 0.05f, groundMask);
-    //}
-
     [SerializeField]
     private float playerSpeed = 2.0f;
     [SerializeField]
@@ -160,6 +15,13 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float rotationSpeed = 50f;
     private Vector3 move;
+    [SerializeField]
+    private GameObject bulletPrefab;
+    [SerializeField]
+    private Transform barrelTransform;
+    [SerializeField]
+    private Transform bulletParent;
+    private LayerMask collisionLayers;
 
 
 
@@ -172,8 +34,9 @@ public class PlayerController : MonoBehaviour
 
     private InputAction moveAction;
     private InputAction jumpAction;
+    private InputAction shootAction;
 
-    private void Start()
+    private void Awake()
     {
         controller = GetComponent<CharacterController>();
         playerInput = GetComponent<PlayerInput>();
@@ -181,6 +44,44 @@ public class PlayerController : MonoBehaviour
         cameraTransform = Camera.main.transform;
         moveAction = playerInput.actions["Move"];
         jumpAction = playerInput.actions["Jump"];
+        shootAction = playerInput.actions["Shoot"];
+    }
+
+    private void OnEnable()
+    {
+        shootAction.performed += _ => ShootGun();
+    }
+
+    private void OnDisable()
+    {
+        shootAction.performed -= _ => ShootGun();
+    }
+    private void ShootGun()
+    {
+        if (!Input.GetKey(KeyCode.Mouse1)) return; // Si no está presionando el botón derecho, no dispara.
+
+        // Instancia la bala en la posición del barril con la rotación de la cámara
+        GameObject bullet = Instantiate(bulletPrefab, barrelTransform.position, Quaternion.identity, bulletParent);
+        BulletController bulletController = bullet.GetComponent<BulletController>();
+
+        if (bulletController != null)
+        {
+            // Determinar la dirección del disparo con un Raycast
+            RaycastHit hit;
+            Vector3 shootDirection;
+
+            if (Physics.Raycast(cameraTransform.position, cameraTransform.forward, out hit, Mathf.Infinity, collisionLayers))
+            {
+                shootDirection = (hit.point - barrelTransform.position).normalized;
+            }
+            else
+            {
+                shootDirection = cameraTransform.forward;
+            }
+
+            // Rotar la bala en la dirección del disparo
+            bullet.transform.rotation = Quaternion.LookRotation(shootDirection);
+        }
     }
 
     void Update()
